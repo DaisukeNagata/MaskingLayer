95e63579f6dd951670bdc6797169f8148dda9161
daisukenagata <dbank0208@gmail.com>
2018-08-30 08:54:58 +0900

    add Preparing for coalescence with mask

--- Example/MaskingLayer/ViewController.swift
+++ Example/MaskingLayer/ViewController.swift
@@ -18,44 +18,44 @@ struct CommonStructure {
 }
 
 class ViewController: UIViewController,UIGestureRecognizerDelegate {
-    
+
     var data = Data()
     var image = UIImage()
     var imageView = UIImageView()
     let maskLayer = MaskLayer()
     var gifObject = GifObject()
     var setVideoURLView = SetVideoURLView()
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         CommonStructure.panGesture = UIPanGestureRecognizer(target: self, action:#selector(panTapped))
         CommonStructure.panGesture.delegate = self
         view.addGestureRecognizer(CommonStructure.panGesture)
-        
+
         CommonStructure.tapGesture = UITapGestureRecognizer(target: self, action:#selector(tapped))
         CommonStructure.tapGesture.delegate = self
         view.addGestureRecognizer(CommonStructure.tapGesture)
-        
+
         CommonStructure.longGesture = UILongPressGestureRecognizer(target: self, action:#selector(longTapeed))
         CommonStructure.longGesture.delegate = self
         view.addGestureRecognizer(CommonStructure.longGesture)
-        
+
         image = UIImage(named: "IMG_4011")!
     }
-    
+
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
         self.view.addSubview( self.imageView)
-        
+
         self.imageView.animateGIF(data: data, duration: Double(2)) {
             guard let image = UIImage(data: self.data) else { return }
             self.imageView.image = image
         }
         maskLayer.imageSet(view: self.view, imageView: self.imageView, image: self.image)
     }
-    
+
     @objc func panTapped(sender:UIPanGestureRecognizer) {
         let position: CGPoint = sender.location(in: imageView)
         switch sender.state {
@@ -80,7 +80,7 @@ class ViewController: UIViewController,UIGestureRecognizerDelegate {
             break
         }
     }
-    
+
     func maskConerPath(position: CGPoint, bool: Bool) { maskLayer.maskConvertPointFromView(viewPoint: position, view: self.view,imageView: imageView,bool:bool) }
     @objc func tapped(sender:UITapGestureRecognizer) {
         guard UIImage(data: self.data) == nil else {
@@ -121,7 +121,7 @@ extension ViewController: UIImagePickerControllerDelegate & UINavigationControll
         SVProgressHUD.dismiss()
         picker.dismiss(animated: true, completion: nil)
     }
-    
+
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         imageView = UIImageView()
         picker.dismiss(animated: true, completion: nil)
