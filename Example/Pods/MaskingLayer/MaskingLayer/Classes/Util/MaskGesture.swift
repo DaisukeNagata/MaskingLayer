8a07359126b581542cacaedad78d13c3c8c7e83b
永田大祐 <dbank0208@gmail.com>
2019-11-09 06:02:08 +0900

    clear import

--- Example/Pods/MaskingLayer/MaskingLayer/Classes/Util/MaskGesture.swift
+++ Example/Pods/MaskingLayer/MaskingLayer/Classes/Util/MaskGesture.swift
@@ -5,8 +5,6 @@
 //  Created by 永田大祐 on 2019/11/01.
 //
 
-import Foundation
-
 struct MaskGesture {
     static var panGesture = UIPanGestureRecognizer()
     static var longGesture = UILongPressGestureRecognizer()
