4f3654855de207110f8aa977a48af23adb74a480
永田大祐 <dbank0208@gmail.com>
2019-11-16 18:24:06 +0900

    change comment

--- Example/MaskingLayer/ViewController+Extension.swift
+++ Example/MaskingLayer/ViewController+Extension.swift
@@ -11,7 +11,7 @@ import MaskingLayer
 extension ViewController {
 
     func alertSave(_ maskLayer: MaskLayer, mo: MaskingLayerViewModel) {
-        let alertController = UIAlertController(title: NSLocalizedString("BackGround Color", comment: ""), message: "", preferredStyle: .alert)
+        let alertController = UIAlertController(title: NSLocalizedString("Camera Option", comment: ""), message: "", preferredStyle: .alert)
         let cameraRoll = UIAlertAction(title: NSLocalizedString("CameraRoll ", comment: ""), style: .default) {
             action in
             alertController.dismiss(animated: true, completion: nil)
@@ -49,47 +49,3 @@ extension ViewController {
         self.present(alertController, animated: true, completion: nil)
     }
 }
-
-/*
- //        let stringAttributes: [NSAttributedString.Key : Any] = [
- //            .foregroundColor : UIColor(red: 0/255, green: 136/255, blue: 83/255, alpha: 1.0),
- //            .font : UIFont.systemFont(ofSize: 22.0)
- //        ]
- //        let string = NSAttributedString(string: alertController.title!, attributes:stringAttributes)
- //        alertController.setValue(string, forKey: "attributedTitle")
- // guard let imageView = mo.imageView else { return }
- alertController.addAction(maskWhite)
- alertController.addAction(maskLightGray)
- alertController.addAction(maskGray)
- alertController.addAction(maskDarkGray)
- alertController.addAction(maskLightBlack)
- let string = NSAttributedString(string: alertController.title!, attributes:stringAttributes)
- alertController.setValue(string, forKey: "attributedTitle")
- alertController.view.tintColor = UIColor(red: 0/255, green: 136/255, blue: 83/255, alpha: 1.0)
- 
- let maskWhite = UIAlertAction(title: NSLocalizedString("MaskWhite", comment: ""), style: .default) {
- action in
- alertController.dismiss(animated: true, completion: nil)
- maskLayer.maskColor = .maskWhite; maskLayer.colorSet(imageView: imageView, image: mo.image, color: maskLayer.maskColor)
- }
- let maskLightGray = UIAlertAction(title: NSLocalizedString("MaskLightGray", comment: ""), style: .default) {
- action in
- alertController.dismiss(animated: true, completion: nil)
- maskLayer.maskColor = .maskLightGray; maskLayer.colorSet(imageView: imageView, image: mo.image, color: maskLayer.maskColor)
- }
- let maskGray = UIAlertAction(title: NSLocalizedString("MaskGray", comment: ""), style: .default) {
- action in
- alertController.dismiss(animated: true, completion: nil)
- maskLayer.maskColor = .maskGray; maskLayer.colorSet(imageView: imageView, image: mo.image, color: maskLayer.maskColor)
- }
- let maskDarkGray = UIAlertAction(title: NSLocalizedString("MaskDarkGray", comment: ""), style: .default) {
- action in
- alertController.dismiss(animated: true, completion: nil)
- maskLayer.maskColor = .maskDarkGray; maskLayer.colorSet(imageView: imageView, image: mo.image, color: maskLayer.maskColor)
- }
- let maskLightBlack = UIAlertAction(title: NSLocalizedString("MaskLightBlack", comment: ""), style: .default) {
- action in
- alertController.dismiss(animated: true, completion: nil)
- maskLayer.maskColor = .maskLightBlack; maskLayer.colorSet(imageView: imageView, image: mo.image, color: maskLayer.maskColor)
- }
- */
