ac6d95e37d67e106411d6a9dc0ad0dbc02b9408a
dbank0208@gmail.com <dbank0208@gmail.com>
2020-11-14 21:41:38 +0900

    PodVersion 1.2.5

--- Example/Pods/MaskingLayer/Pods/MaskingLayer/Classes/ViwModel/SliiderObjectsView.swift
+++ Example/Pods/MaskingLayer/Pods/MaskingLayer/Classes/ViwModel/SliiderObjectsView.swift
@@ -51,7 +51,7 @@ class SliiderObjectsView: UIView, UIGestureRecognizerDelegate {
     }
 
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
 
     func loadNib() {
